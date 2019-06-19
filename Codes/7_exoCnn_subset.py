5de72e9f5276ace7a6c45cd51cf3babf1b1ff910
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-06-19 19:21:34 +0430

    Add subset for CNN

--- Codes/7_exoCnn_subset.py
+++ Codes/7_exoCnn_subset.py
@@ -1,6 +1,5 @@
-## 6_exoMLP
+## 7_exoCnn_subset
 ## includes only the independent features
-
 
 
 import argparse
@@ -9,18 +8,15 @@ import numpy as np
 import pandas as pd
 from keras import backend as K
 from keras.callbacks import EarlyStopping, CSVLogger
-from keras.layers import Conv1D, Dense, Flatten, Input, MaxPooling1D, BatchNormalization, Dropout, LeakyReLU, \
-    concatenate, LSTM
+from keras.layers import Conv1D, Dense, Flatten, Input, MaxPooling1D, BatchNormalization, Dropout, LeakyReLU
 from keras.models import Model
-from keras.optimizers import Adam, Nadam
-from keras.preprocessing.sequence import pad_sequences
-from keras.regularizers import l2, l1
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, normalize
+from keras.optimizers import Adam
+from keras.regularizers import l2
 from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
-
-model_name = '6_exoMlp_subset'
+model_name = '7_exoCnn_subset'
 parser = argparse.ArgumentParser(description='The higher order of ZarNet!!')
 arguments_group = parser.add_argument_group("Parameters")
 arguments_group.add_argument('-r', '--learning_rate', type=float, default=0.002, required=False,
@@ -38,11 +34,10 @@ arguments_group.add_argument('-d', '--dropout_rate', type=float, default=0.4, re
 arguments_group.add_argument('-w', '--weight', type=float, default=0.1, required=False,
                              help='class weight for Label 1')
 
-
 args = vars(parser.parse_args())
 
 testsize = 0.25
-subsample_size = 5000
+subsample_size = 2000
 learning_rate = args['learning_rate']
 lambda_value = args['lambda']
 dropout_rate = args['dropout_rate']
@@ -51,59 +46,65 @@ batch_size = args['batch_size']
 epochs = args['epochs']
 weight = args['weight']
 
-
-
-## loading data and initial preprocessing 
+## loading data and initial preprocessing
 data = pd.read_csv("../Data/MergedDesignMatLabel_SecondStruct_LenFilter_forgi_element_string.csv")
-deepbind = pd.read_csv('../Data/deepbind_features.txt',sep='\t')
-print(data.shape)
-
-data = pd.concat([data, deepbind], axis=1)
-rowsToRemove = [i for i in range(len(data['seq'])) if set(data['seq'][i]) == {'C', 'A', 'G', 'U', 'N'} ]
+rowsToRemove = [i for i in range(len(data['seq'])) if set(data['seq'][i]) == {'C', 'A', 'G', 'U', 'N'}]
 data.drop(rowsToRemove, axis=0, inplace=True)
-data.drop(['Unnamed: 0', 'id', 'annotation', 'rnaType', 'ic', 'ev', 'seq', 'DB', 'element_string','element_string_number'], axis=1, inplace=True)
 data = data.reset_index()
 data.drop('index', axis=1, inplace=True)
-print(data.shape)
 
-
-
-def preprocess_mlp(data):
-    count_features = data.columns.tolist()[data.columns.tolist().index('AAAA'):]
-    for column in count_features:
-        data[column] /= data['length']
-
-    count_features_df = data.drop(['strand', 'chr'], axis=1)
-    count_features_df = pd.DataFrame(normalize(count_features_df.values, norm='max'), columns=count_features_df.columns)
-
-    strand = data['strand']
-    le = LabelEncoder()
-    data['strand'] = pd.Series(le.fit_transform(strand.values))
-
-    chromosome = data['chr']
-    le = LabelEncoder()
-    data['chr'] = pd.Series(le.fit_transform(chromosome.values))
-
-    return pd.concat([data['chr'], data['strand'], count_features_df], axis=1)
-
-mlp_data = preprocess_mlp(data.drop(['label'], axis=1))
-
-
-
-## prepare the labels
-
+## preprocess cnn input data
+sequences = data['seq']
+dotbracket = data['DB']
+element = data['element_string']
+max_len = sequences.str.len().max()
 labels = data['label']
 labels = np.array(labels)
 labels[labels == 'NO'] = 0
 labels[labels == 'YES'] = 1
 
+print('max_len: ', max_len)
+
+
+## encoding and mergeing the sequance features for CNN
+def encoder(VectorOfStrings, charsToFit, max_len):
+    ## integer encoding
+    le = LabelEncoder()
+    le.fit(list(charsToFit))
+    integer_encoded = [le.transform(list(VectorOfStrings[i])).tolist() for i in range(VectorOfStrings.shape[0])]
+
+    ## one-hot encoding
+    oe = OneHotEncoder(categories='auto')
+    oe = oe.fit(np.reshape(list(range(len(charsToFit))), (-1, 1)))
+    onehot_encoded = [oe.transform(np.reshape(integer_encoded[i], (-1, 1))).A for i in range(len(integer_encoded))]
+    onehot_encoded = np.array(onehot_encoded)
+
+    def _add_pad(max_len, a_onehot, charsToFit):
+        pad = np.zeros((max_len - len(a_onehot), len(charsToFit)))
+        a_padded_onehot = np.concatenate((pad, a_onehot))
+        return (a_padded_onehot)
+
+    onehot_encoded_padded = [_add_pad(max_len, onehot_encoded[i], charsToFit) for i in range(len(onehot_encoded))]
+    onehot_encoded_padded = np.array(onehot_encoded_padded)
+    print('shape after padding', onehot_encoded_padded.shape)
+
+    return (onehot_encoded_padded)
+
+
+encoded_sequences = encoder(sequences, 'AUCG', max_len)
+dotbracket = dotbracket.str.replace(')', '(', regex=False)
+encoded_dotbracket = encoder(dotbracket, '.(', max_len)
+encoded_element = encoder(element, 'ftsimh', max_len)
+merged_sequences = np.concatenate((encoded_sequences, encoded_dotbracket, encoded_element), axis=2)
+print(merged_sequences.shape)
+
 
 ## defining performance measures
-
 def sensitivity(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     return true_positives / (possible_positives + K.epsilon())
+
 
 def specificity(y_true, y_pred):
     true_negatives = K.sum(K.round(K.clip((1 - y_true) * (1 - y_pred), 0, 1)))
@@ -111,54 +112,74 @@ def specificity(y_true, y_pred):
     return true_negatives / (possible_negatives + K.epsilon())
 
 
+def cnn(seq_len, onehot_len):
+    cnn_input = Input(shape=(seq_len, onehot_len,))
 
+    conv = Conv1D(filters=8, kernel_size=8, padding='same', kernel_regularizer=l2(lambda_value))(cnn_input)
+    conv = BatchNormalization()(conv)
+    conv = LeakyReLU()(conv)
+    max_pool = MaxPooling1D(pool_size=2)(conv)
 
-def exo_mlp(num_features):
-    mlp_input = Input(shape=(num_features,))
+    conv = Conv1D(filters=16, kernel_size=5, padding='same', kernel_regularizer=l2(lambda_value))(max_pool)
+    conv = BatchNormalization()(conv)
+    conv = LeakyReLU()(conv)
+    max_pool = MaxPooling1D(pool_size=2)(conv)
 
-    dense = Dense(128, activation='linear', kernel_regularizer=l2(lambda_value))(mlp_input)
+    conv = Conv1D(filters=16, kernel_size=3, padding='same', kernel_regularizer=l2(lambda_value))(max_pool)
+    conv = BatchNormalization()(conv)
+    conv = LeakyReLU()(conv)
+    max_pool = MaxPooling1D(pool_size=2)(conv)
+
+    conv = Conv1D(filters=16, kernel_size=3, padding='same', kernel_regularizer=l2(lambda_value))(max_pool)
+    conv = BatchNormalization()(conv)
+    conv = LeakyReLU()(conv)
+    max_pool = MaxPooling1D(pool_size=2)(conv)
+
+    flat = Flatten()(max_pool)
+
+    dense = Dense(128, kernel_regularizer=l2(lambda_value))(flat)
     dense = BatchNormalization()(dense)
     dense = LeakyReLU()(dense)
     dense = Dropout(dropout_rate)(dense)
 
-    dense = Dense(32, activation='linear', kernel_regularizer=l2(lambda_value))(dense)
+    dense = Dense(32, kernel_regularizer=l2(lambda_value))(dense)
     dense = BatchNormalization()(dense)
     dense = LeakyReLU()(dense)
     dense = Dropout(dropout_rate)(dense)
 
     output = Dense(1, activation='sigmoid')(dense)
 
-    model = Model(inputs=mlp_input, outputs=output)
-    model.compile(optimizer=Nadam(lr=learning_rate), loss='binary_crossentropy',
+    model = Model(inputs=cnn_input, outputs=output)
+    model.compile(optimizer=Adam(lr=learning_rate), loss='binary_crossentropy',
                   metrics=['acc', sensitivity, specificity])
     model.summary()
-
     return model
 
 
-model = exo_mlp(mlp_data.shape[1])
+model = cnn(max_len, merged_sequences.shape[2])
 
 # Subsampling
-mlp_data_no = mlp_data.values[labels == 0]
-mlp_data_yes = mlp_data.values[labels == 1]
+merged_sequences_no = merged_sequences[labels == 0]
+merged_sequences_yes = merged_sequences[labels == 1]
 
 labels_no = labels[labels == 0]
 labels_yes = labels[labels == 1]
 
-random_indices = np.random.choice(mlp_data_no.shape[0], subsample_size, replace=True)
-mlp_data_no = mlp_data_no[random_indices]
+random_indices = np.random.choice(merged_sequences.shape[0], subsample_size, replace=True)
+merged_sequences_no = merged_sequences_no[random_indices]
 labels_no = labels_no[random_indices]
 
-mlp_data = np.concatenate([mlp_data_no, mlp_data_yes], axis=0)
+merged_sequences = np.concatenate([merged_sequences_no, merged_sequences_yes], axis=0)
 labels = np.concatenate([labels_no, labels_yes], axis=0)
 
 # Train/Test Split
-x_train, x_test, y_train, y_test = train_test_split(mlp_data, labels, test_size=testsize, stratify=labels)
+x_train, x_test, y_train, y_test = train_test_split(merged_sequences, labels, test_size=testsize,
+                                                    stratify=data[["label"]])
 print(x_train.shape, y_train.shape)
 print(x_test.shape, y_test.shape)
 
 early_stopping = EarlyStopping(patience=patience, monitor='val_loss', mode='min')
-csv_logger = CSVLogger(filename="./"+model_name+"_train.log")
+csv_logger = CSVLogger(filename="./" + model_name + "_train.log")
 
 model.fit(x=x_train,
           y=y_train,
@@ -170,7 +191,7 @@ model.fit(x=x_train,
           callbacks=[early_stopping, csv_logger],
           shuffle=True)
 
-model.save("./"+model_name+".h5")
+model.save("./" + model_name + ".h5")
 
 
 def visualize_results():
